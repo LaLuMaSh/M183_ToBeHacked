c553ba6b417d2fd561810fff9084075e4790d1d9
taakesh1 <shawn.keller@swisscom.com>
2020-08-18 10:50:09 +0200

    Luca stinkt heavy;Luca stinkt heavy;Luca stinkt heavy;Luca stinkt heavy;Luca stinkt heavy;Luca stinkt heavy;Luca stinkt heavy;

---
+++ ToBeHacked/src/main/java/com/lucastinkt/ToBeHacked/ToBeHackedApplication.java
@@ -0,0 +1,13 @@
+package com.lucastinkt.ToBeHacked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToBeHackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToBeHackedApplication.class, args);
+	}
+
+}
---
+++ ToBeHacked/src/test/java/com/lucastinkt/ToBeHacked/ToBeHackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucastinkt.ToBeHacked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToBeHackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

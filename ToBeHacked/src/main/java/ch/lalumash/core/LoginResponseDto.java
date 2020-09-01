0cda228d1629a53a0bb33cb74d8b02096c2f3db3
LucaGuettinger <luca.guettinger@cmiag.ch>
2020-09-01 09:03:35 +0200

    remove lombok

--- ToBeHacked/src/main/java/ch/lalumash/core/LoginResponseDto.java
+++ ToBeHacked/src/main/java/ch/lalumash/core/LoginResponseDto.java
@@ -4,9 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@Data
-@NoArgsConstructor
 public class LoginResponseDto {
+    public LoginResponseDto() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LoginResponseDto(String token) {
+        this.token = token;
+    }
+
     String token;
 }
